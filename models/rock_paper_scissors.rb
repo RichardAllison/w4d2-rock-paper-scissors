808a4c036fca1383730a6d7ba865d74c8f8ab9fb
Richard Allison <richard.allison.18@outlook.com>
2017-12-12 22:27:22 +0000

    Updated the model to return which player wins rather than which hand wins, and added rules to home page.

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -6,27 +6,56 @@ class RockPaperScissorsGame
   end
 
   def play()
-    if (@move1 == 'rock' || @move1 == 'paper' || @move1 == 'scissors') && (@move2 == 'rock' || @move2 == 'paper' || @move2 == 'scissors')
-      if (@move1 == 'rock' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'rock')
-        return 'paper!'
-      elsif (@move1 == 'rock' && @move2 == 'scissors') || (@move1 == 'scissors' && @move2 == 'rock')
-        return 'rock!'
-      elsif @move1 == 'rock' && @move2 == 'rock'
-        return 'It\'s a draw!'
-      elsif (@move1 == 'scissors' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'scissors')
-        return 'scissors!'
-      elsif @move1 == 'scissors' && @move2 == 'scissors'
-        return 'It\'s a draw!'
-      elsif @move1 == 'paper' && @move2 == 'paper'
-        return 'It\'s a draw!'
+    if (@move1 == "rock" || @move1 == "paper" || @move1 == "scissors") && (@move2 == "rock" || @move2 == "paper" || @move2 == "scissors")
+      if @move1 == "rock" && @move2 == "paper"
+        return "player 2!"
+      elsif @move1 == "paper" && @move2 == "rock"
+        return "player 1!"
+      elsif @move1 == "rock" && @move2 == "scissors"
+        return "player 1!"
+      elsif @move1 == "scissors" && @move2 == "rock"
+        return "player 2!"
+      elsif @move1 == "scissors" && @move2 == "paper"
+        return "player 1!"
+      elsif @move1 == "paper" && @move2 == "scissors"
+        return "player 2!"
+      elsif @move1 == "rock" && @move2 == "rock"
+        return "It\'s a draw!"
+      elsif @move1 == "scissors" && @move2 == "scissors"
+        return "It\'s a draw!"
+      elsif @move1 == "paper" && @move2 == "paper"
+        return "It\'s a draw!"
       end
-    elsif (@move1 == 'rock' || @move1 == 'paper' || @move1 == 'scissors')
+    elsif (@move1 == "rock" || @move1 == "paper" || @move1 == "scissors")
       return "#{@move2.capitalize()} is not a valid move!"
-    elsif (@move2 == 'rock' || @move2 == 'paper' || @move2 == 'scissors')
+    elsif (@move2 == "rock" || @move2 == "paper" || @move2 == "scissors")
       return "#{@move1.capitalize()} is not a valid move!"
     else
       return "Neither move is valid!"
     end
+
+  # def play()
+  #   if (@move1 == "rock" || @move1 == "paper" || @move1 == "scissors") && (@move2 == "rock" || @move2 == "paper" || @move2 == "scissors")
+  #     if (@move1 == "rock" && @move2 == "paper") || (@move1 == "paper" && @move2 == "rock")
+  #       return "paper!"
+  #     elsif (@move1 == "rock" && @move2 == "scissors") || (@move1 == "scissors" && @move2 == "rock")
+  #       return "rock!"
+  #     elsif @move1 == "rock" && @move2 == "rock"
+  #       return "It\'s a draw!"
+  #     elsif (@move1 == "scissors" && @move2 == "paper") || (@move1 == "paper" && @move2 == "scissors")
+  #       return "scissors!"
+  #     elsif @move1 == "scissors" && @move2 == "scissors"
+  #       return "It\'s a draw!"
+  #     elsif @move1 == "paper" && @move2 == "paper"
+  #       return "It\'s a draw!"
+  #     end
+  #   elsif (@move1 == "rock" || @move1 == "paper" || @move1 == "scissors")
+  #     return "#{@move2.capitalize()} is not a valid move!"
+  #   elsif (@move2 == "rock" || @move2 == "paper" || @move2 == "scissors")
+  #     return "#{@move1.capitalize()} is not a valid move!"
+  #   else
+  #     return "Neither move is valid!"
+  #   end
 
   end
 
