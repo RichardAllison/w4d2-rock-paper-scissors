97185f5725f1cc9b1ecdb0eb07ef28c5267040c0
Richard Allison <richard.allison.18@outlook.com>
2017-12-12 21:43:36 +0000

    Created layout.erb with navigation bar and links, and added home page and about page with routes to get them

--- controller.rb
+++ controller.rb
@@ -3,6 +3,14 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/rock_paper_scissors')
 
+get('/') do
+  erb(:home)
+end
+
+get('/about') do
+  erb(:about)
+end
+
 get('/play/:move1/:move2') do
   game = RockPaperScissorsGame.new(params['move1'], params['move2'])
   @result = game.play()
--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -4,69 +4,69 @@ require_relative('../models/rock_paper_scissors')
 
 class TestRockPaperScissorsGame < Minitest::Test
 
-  # def test_rock_paper()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'paper')
-  #   assert_equal( "Paper wins!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_paper_rock()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'rock')
-  #   assert_equal( "Paper wins!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_rock_scissors()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'scissors')
-  #   assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_scissors_rock()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'rock')
-  #   assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_rock_rock()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'rock')
-  #   assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_paper_scissors()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'scissors')
-  #   assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_scissors_paper()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'paper')
-  #   assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_paper_paper()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'paper')
-  #   assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_scissors_scissors()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'scissors')
-  #   assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_rock_dynamite()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'dynamite')
-  #   assert_equal( "Dynamite is not a valid move", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_jackhammer_rock()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'rock')
-  #   assert_equal( "Jackhammer is not a valid move", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_dynamite_jackhammer()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('dynamite', 'jackhammer')
-  #   assert_equal( "Neither dynamite nor jackhammer is a valid move!", rock_paper_scissors_game.play() )
-  # end
-  #
-  # def test_jackhammer_dynamite()
-  #   rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'dynamite')
-  #   assert_equal( "Neither jackhammer nor jackhammer is a valid move!", rock_paper_scissors_game.play() )
-  # end
+  def test_rock_paper()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'paper')
+    assert_equal( "Paper wins!", rock_paper_scissors_game.play() )
+  end
+
+  def test_paper_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'rock')
+    assert_equal( "Paper wins!", rock_paper_scissors_game.play() )
+  end
+
+  def test_rock_scissors()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'scissors')
+    assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
+  end
+
+  def test_scissors_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'rock')
+    assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
+  end
+
+  def test_rock_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'rock')
+    assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
+  end
+
+  def test_paper_scissors()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'scissors')
+    assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
+  end
+
+  def test_scissors_paper()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'paper')
+    assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
+  end
+
+  def test_paper_paper()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'paper')
+    assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
+  end
+
+  def test_scissors_scissors()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'scissors')
+    assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
+  end
+
+  def test_rock_dynamite()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'dynamite')
+    assert_equal( "Dynamite is not a valid move", rock_paper_scissors_game.play() )
+  end
+
+  def test_jackhammer_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'rock')
+    assert_equal( "Jackhammer is not a valid move", rock_paper_scissors_game.play() )
+  end
+
+  def test_dynamite_jackhammer()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('dynamite', 'jackhammer')
+    assert_equal( "Neither dynamite nor jackhammer is a valid move!", rock_paper_scissors_game.play() )
+  end
+
+  def test_jackhammer_dynamite()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'dynamite')
+    assert_equal( "Neither jackhammer nor jackhammer is a valid move!", rock_paper_scissors_game.play() )
+  end
 
 end
