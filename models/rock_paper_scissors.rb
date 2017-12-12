2f050107b10143a61abc850c23ff405d964c3490
Richard Allison <richard.allison.18@outlook.com>
2017-12-12 20:15:54 +0000

    Completed RockPaperScissorsGame class with all combinations tested including invalid combinations, all tests passed

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -6,15 +6,28 @@ class RockPaperScissorsGame
   end
 
   def play()
-    if (@move1 == 'rock' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'rock')
-      return 'Paper wins!'
-    elsif (@move1 == 'rock' && @move2 == 'scissors') || (@move1 == 'scissors' && @move2 == 'rock')
-      return 'Rock wins!'
-    elsif @move1 == 'rock' && @move2 == 'rock'
-      return 'It\'s a draw!'
-    elsif (@move1 == 'scissors' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'scissors')
-      return 'Scissors win!'
+    if (@move1 == 'rock' || @move1 == 'paper' || @move1 == 'scissors') && (@move2 == 'rock' || @move2 == 'paper' || @move2 == 'scissors')
+      if (@move1 == 'rock' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'rock')
+        return 'Paper wins!'
+      elsif (@move1 == 'rock' && @move2 == 'scissors') || (@move1 == 'scissors' && @move2 == 'rock')
+        return 'Rock wins!'
+      elsif @move1 == 'rock' && @move2 == 'rock'
+        return 'It\'s a draw!'
+      elsif (@move1 == 'scissors' && @move2 == 'paper') || (@move1 == 'paper' && @move2 == 'scissors')
+        return 'Scissors win!'
+      elsif @move1 == 'scissors' && @move2 == 'scissors'
+        return 'It\'s a draw!'
+      elsif @move1 == 'paper' && @move2 == 'paper'
+        return 'It\'s a draw!'
+      end
+    elsif (@move1 == 'rock' || @move1 == 'paper' || @move1 == 'scissors')
+      return "#{@move2.capitalize()} is not a valid move"
+    elsif (@move2 == 'rock' || @move2 == 'paper' || @move2 == 'scissors')
+      return "#{@move1.capitalize()} is not a valid move"
+    else
+      return "Neither #{@move1} nor #{@move2} is a valid move!"
     end
+
   end
 
 end
--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -19,6 +19,11 @@ class TestRockPaperScissorsGame < Minitest::Test
     assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
   end
 
+  def test_scissors_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'rock')
+    assert_equal( "Rock wins!", rock_paper_scissors_game.play() )
+  end
+
   def test_rock_rock()
     rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'rock')
     assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
@@ -27,6 +32,41 @@ class TestRockPaperScissorsGame < Minitest::Test
   def test_paper_scissors()
     rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'scissors')
     assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
+  end
+
+  def test_scissors_paper()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'paper')
+    assert_equal( "Scissors win!", rock_paper_scissors_game.play() )
+  end
+
+  def test_paper_paper()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('paper', 'paper')
+    assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
+  end
+
+  def test_scissors_scissors()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('scissors', 'scissors')
+    assert_equal( "It's a draw!", rock_paper_scissors_game.play() )
+  end
+
+  def test_rock_dynamite()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('rock', 'dynamite')
+    assert_equal( "Dynamite is not a valid move", rock_paper_scissors_game.play() )
+  end
+
+  def test_jackhammer_rock()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'rock')
+    assert_equal( "Jackhammer is not a valid move", rock_paper_scissors_game.play() )
+  end
+
+  def test_dynamite_jackhammer()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('dynamite', 'jackhammer')
+    assert_equal( "Neither dynamite nor jackhammer is a valid move!", rock_paper_scissors_game.play() )
+  end
+
+  def test_jackhammer_dynamite()
+    rock_paper_scissors_game = RockPaperScissorsGame.new('jackhammer', 'dynamite')
+    assert_equal( "Neither jackhammer nor dynamite is a valid move!", rock_paper_scissors_game.play() )
   end
 
 end
